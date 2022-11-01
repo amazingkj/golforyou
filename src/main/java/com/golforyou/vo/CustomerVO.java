dc0c1a5a701bc474567e822ac26fae800b5ed907
kim\kim <jiin724@gmail.com>
2022-11-02 00:00:04 +0900

    1:1답변에 글 남기면 담당자에게 메일 발송_첨부파일 제외 (김지인)

---
+++ src/main/java/com/golforyou/vo/CustomerVO.java
@@ -0,0 +1,16 @@
+package com.golforyou.vo;
+
+
+import lombok.Data;
+
+@Data
+public class CustomerVO {
+	
+	private String consult_type;
+	private String question_title;
+	private String question_contents;
+	private String phoneNum;
+	private String question_email;
+	
+
+}
